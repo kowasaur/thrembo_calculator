ae7c194829950960c60cd70fb89249423d3e4ec8
kowasaur <75297283+kowasaur@users.noreply.github.com>
2022-08-13 17:28:26 +1000

    addition

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const _normalNumber = {
+  static const _thremboToNormal = {
     '0': 0,
     '1': 1,
     '2': 2,
@@ -46,49 +46,87 @@ class _MyHomePageState extends State<MyHomePage> {
     '9': 10
   };
 
+  // Reverse of _thremboToNormal
+  static final _normalToThrembo =
+      _thremboToNormal.map((key, value) => MapEntry(value, key));
+
   String _thremboText = "";
 
-  void _inputChar(String char) => setState(() => _thremboText += char);
+  void _setThremboText(String str) => setState(() => _thremboText = str);
 
-  void _deleteChar() => setState((() {
-        var lastIndex = _thremboText.length - 1;
-        if (lastIndex != -1) {
-          _thremboText = _thremboText.substring(0, lastIndex);
-        }
-      }));
+  void _inputChar(String char) => _setThremboText(_thremboText + char);
+
+  void _deleteChar() {
+    final lastIndex = _thremboText.length - 1;
+    if (lastIndex != -1) {
+      _setThremboText(_thremboText.substring(0, lastIndex));
+    }
+  }
 
   CalcButton _charButton(String char) {
     return CalcButton(onPressed: () => _inputChar(char), child: Text(char));
   }
 
-  static String _convertNumber(String thremboNumber) {
+  static int _convertNumber(String thremboNumber) {
     int resultNum = 0;
     for (var i = 0; i < thremboNumber.length; i++) {
       var char = thremboNumber[thremboNumber.length - 1 - i];
-      resultNum += _normalNumber[char]! * pow(11, i) as int;
+      resultNum += _thremboToNormal[char]! * pow(11, i) as int;
     }
-    return resultNum.toString();
+    return resultNum;
   }
 
   /// Check whether the _thremboText character at this index is a thrembo number
-  bool indexIsNum(int index) =>
+  bool _indexIsNum(int index) =>
       index != _thremboText.length &&
-      _normalNumber.keys.contains(_thremboText[index]);
+      _thremboToNormal.keys.contains(_thremboText[index]);
 
-  String _convertExpression() {
-    String convertedExpression = "";
+  List<dynamic> _parseExpression() {
+    final tokens = [];
     for (var i = 0; i < _thremboText.length; i++) {
       String buffer = _thremboText[i];
-      if (indexIsNum(i)) {
-        while (indexIsNum(i + 1)) {
+      if (_indexIsNum(i)) {
+        while (_indexIsNum(i + 1)) {
           buffer += _thremboText[++i];
         }
-        convertedExpression += _convertNumber(buffer);
+        tokens.add(_convertNumber(buffer));
       } else {
-        convertedExpression += buffer;
+        tokens.add(buffer);
       }
     }
-    return convertedExpression;
+    return tokens;
+  }
+
+  String _convertExpression() =>
+      _parseExpression().fold("", (str, token) => str + token.toString());
+
+  void _evaluateExpression() {
+    final tokens = _parseExpression();
+    while (tokens.length != 1) {
+      for (var i = 0; i < tokens.length; i++) {
+        // If it's an operator
+        if (tokens[i].runtimeType == String) {
+          final int left = tokens[i - 1];
+          final int right = tokens[i + 1];
+          final int result = left + right;
+          tokens.removeRange(i, i + 2); // [i, i+2)
+          tokens[i - 1] = result;
+          break;
+        }
+      }
+    }
+    final int answer = tokens[0];
+
+    // Convert to thrembo number
+    int remaining = answer;
+    String buffer = "";
+    while (true) {
+      final remainder = remaining % 11;
+      buffer = _normalToThrembo[remainder]! + buffer;
+      if ((remaining - remainder) < 11) break;
+      remaining = (remaining - remainder) ~/ 11;
+    }
+    _setThremboText(buffer);
   }
 
   @override
@@ -108,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _charButton('8'),
       _charButton('9'),
       CalcButton(onPressed: _deleteChar, child: const Icon(Icons.backspace)),
-      CalcButton(onPressed: () => debugPrint("equals"), child: const Text('='))
+      CalcButton(onPressed: _evaluateExpression, child: const Text('='))
     ]);
 
     return Scaffold(
